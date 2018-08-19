13a4b452a092787bfcce43166bfc9e1887d49af3
Ian Gornall <ian.gornall@gmail.com>
2018-08-19 12:42:48 -0400

    Started working on this.

---
+++ scripts/data.js
@@ -0,0 +1,12 @@
+let data = {
+  games: false,
+  games_reason: false,
+  books: false,
+  books_reason: false,
+  music: false,
+  music_reason: false,
+  movies: false,
+  movies_reason: false,
+  social: false,
+  social_reason: false
+}
--- scripts/dragdrop.js
+++ scripts/dragdrop.js
@@ -2,7 +2,7 @@ let items = document.querySelectorAll('.item');
 for (let item of items){
   item.ondragstart = event => {
     console.log(event.target.id);
-    event.dataTransfer.setData("dragElement", event.target.id);
+    event.dataTransfer.setData("dragElement", event.currentTarget.id);
     event.effectAllowed = "copyMove";
   }
 }
@@ -11,7 +11,10 @@ let dropHandler = event => {
   console.log('dropped.');
   let dragId = event.dataTransfer.getData("dragElement");
   let dragElement = document.querySelector('#' + dragId);
-  event.target.appendChild(dragElement);
+  event.currentTarget.appendChild(dragElement);
+  let choice = dragId.replace('-choice', '');
+  data[choice] = true;
+  console.log(data);
 }
 let dragOff = event => {
   event.preventDefault();
