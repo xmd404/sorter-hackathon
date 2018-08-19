9a39c5f84c89cc3bc3d5b81aaae722b1a57df11a
Ian Gornall <ian.gornall@gmail.com>
2018-08-19 11:57:54 -0400

    Timing Fix.

--- scripts/textbox.js
+++ scripts/textbox.js
@@ -2,7 +2,7 @@ let textArray = ["Good evening Earthling!",
                 "As the last of your kind - the robot people of the Omicron-Setcon 8 would like to offer you a proposition...",
                 "We would like to transport you to a new organic based civilization, but you can only bring 3 relics of humanity with you...",
                 "You may bring the best of humanity's literature, games, music, movies or social media",
-                "You may only choose three.  You must tell us why.  You only have 2 minutes to decide before Earth implodes."]
+                "You may only choose three.  You must tell us why.  You only have 2 minutes to decide before Earth implodes.", "Good luck.  Press Start to begin."]
 
 let bubble = document.querySelector(".textbubble");
 
@@ -18,10 +18,8 @@ for (i=0; i<textArray.length; i++) {
         text.classList.add("thirdPrompt");
     } else if (i === 3) {
         text.classList.add("fourthPrompt");
-    } else if (i === 4) {}
-    let drawText = function() {
-        bubble.appendChild(text);
+    } else if (i === 4) {
+        text.classList.add('fifthPrompt');
     }
-    setInterval(drawText, 5000);
-        
+    bubble.appendChild(text);
 }
