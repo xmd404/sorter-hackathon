c54f442ef977b2f2f30b95a0195e7e763679c061
Ian Gornall <ian.gornall@gmail.com>
2018-08-25 15:41:51 -0400

    adding facebook auth

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+const env = require('dotenv').config();
 const user = require('./user');  // Create a user.js file in this directory with your username to test database.
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 const app = express();
 const pg = require('pg-promise')();
 const dbConfig = 'postgres://' + user + '@localhost:5432/';
 const db = pg(dbConfig);
+const PORT = process.env.PORT || 3000; // Get port from environment
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/login/return"
+},
+function(accessToken, refreshToken, profile, cb) {
+  // Insert user profile into database, or load if existing.
+  // In this example, the user's Facebook profile is supplied as the user
+  // record.  In a production-quality application, the Facebook profile should
+  // be associated with a user record in the application's database, which
+  // allows for account linking and authentication with other identity
+  // providers.
+  return cb(null, profile);
+}));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static('public'));
 
-app.listen(3000);
+app.get('/login',
+  passport.authenticate('facebook'));
+
+app.get('/login/return', 
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.listen(PORT);
 
